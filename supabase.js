7a696d3294162bf4a789e44c18fa1d35854c8159
6z85rdj2mh-wq <6z85rdj2mh@privaterelay.appleid.com>
2026-07-21 18:28:24 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://usrlanvyuaaemdxhhuvg.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://usrlanvyuaaemdxhhuvg.supabase.co";
 
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzcmxhbnZ5dWFhZW1keGhodXZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2NDMzNTksImV4cCI6MjEwMDIxOTM1OX0.9VZfPoyYwbE4rv48LYHx0orpda4Jd5G7P8n1tVT9nPw";
 
